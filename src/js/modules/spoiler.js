6fe2f3b304f27ce324ce87c0668137835540e033
Kolosov Arkady <arkolosov2020@yandex.ru>
2022-05-19 11:36:41 +0300

    added any style and link to project

--- src/js/modules/spoiler.js
+++ src/js/modules/spoiler.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('.menu__button').click(function (event) {
-    if ($('.block-name').hasClass('one')) {
+    if ($('.menu__list').hasClass('one')) {
       $('.menu__links').not($(this)).removeClass('active');
       $('.menu__links').not($(this).next()).slideUp(300);
     }
