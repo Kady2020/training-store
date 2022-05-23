1e308a76d4c97289d7b9a3e2089f6a976111eb82
Kolosov Arkady <arkolosov2020@yandex.ru>
2022-05-23 16:26:25 +0300

    test commit

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,6 @@ import * as flsFunctions from "./modules/webp.js";
 flsFunctions.isWebp();
 
 import "./libraries/jquery-3.6.0.min.js";
-import "./modules/swiper.js";
-import "./modules/header-menu.js"
-import "./modules/spoiler.js"
+import "./modules/spoiler.js";
+import "./modules/search.js";
+import "./modules/header-scroll.js";
